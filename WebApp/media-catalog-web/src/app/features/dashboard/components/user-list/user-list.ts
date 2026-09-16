3ab091c8c6c66491424daf59cab224af98bd4898
marcos <marcosbuydid@gmail.com>
2026-09-15 23:53:46 -0300

    add minor change

--- WebApp/media-catalog-web/src/app/features/dashboard/components/user-list/user-list.ts
+++ WebApp/media-catalog-web/src/app/features/dashboard/components/user-list/user-list.ts
@@ -4,12 +4,11 @@ import { UserDetailDTO } from '../../../auth/models/user-detail.dto';
 import { filter, map, Observable, of, switchMap, take } from 'rxjs';
 import { SessionService } from '../../../../core/services/session.service';
 import { UserService } from '../../../users/user.service';
-import { EditUser } from '../edit-user/edit-user';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
-  imports: [CommonModule, EditUser, RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css',
 })
